1227cd81090d60a914cd6c4bec7b106a0e2f6a10
DESKTOP-N1P36QT\Ignasi <ignasiguillen@gmail.com>
2017-10-06 01:12:13 +0200

    Slime movement, amb funcio setBufferAndPlay, un cop entra buffer slime es mou

---
+++ SlimeGame/Slimes/Assets/Scripts/SlimeMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeMovement : MonoBehaviour {
+
+	private Vector2 startPos, endPos;
+	private List<Vector2> bufferPositions = null;
+	private int indexBuffer;
+
+	public float secondsXmovement = 1; //1 segon
+	private float startTime;
+
+	void Start(){
+		List<Vector2> list = new List<Vector2> ();
+		list.Add(new Vector2(4,4));
+		list.Add(new Vector2(4,-4));
+		list.Add(new Vector2(-4,-4));
+		list.Add(new Vector2(-4,4));
+
+		SetBufferAndPlay (list);
+	}
+	
+	// Update is called once per frame
+
+	void Update(){
+		if (bufferPositions != null && bufferPositions.Count>0) {
+			float i = (Time.time - startTime) / secondsXmovement;
+			transform.position = Vector2.Lerp (startPos, endPos, i);
+			if (i >= 1) {
+				startTime = Time.time;
+				indexBuffer++;
+				if (indexBuffer >= bufferPositions.Count) {
+					bufferPositions = null;
+				} else {
+					startPos = endPos;
+					endPos = bufferPositions [indexBuffer];
+				}
+			}
+		}
+	}
+
+	void SetBufferAndPlay(List<Vector2> buffer){
+		if (buffer != null && buffer.Count > 0) {
+			bufferPositions = buffer;
+			startPos = transform.position;
+			startTime = Time.time;
+			indexBuffer = 0;
+			endPos = bufferPositions [indexBuffer];
+		}
+	}
+}
