b4c4f1e3c70bef2789eca5de8811f08ca5a1ac21
barquerogerman <barquerogerman@gmail.com>
2017-10-06 18:15:48 +0200

    Implementado el esqueleto del MapParser.

---
+++ SlimeGame/Slimes/Assets/Utils/MapParser.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapParser : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ReadMap ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Update of MapParser...", gameObject);
+	}
+
+	void ReadMap() {
+		int[][] map;
+
+		string path = "Assets/Resources/map.txt";
+
+		System.IO.StreamReader reader = new System.IO.StreamReader (path);
+		string line = reader.ReadLine ();
+		while (line != null) {
+			string[] elements = line.Split (' ');
+			if (elements.Length > 0) {
+				// Convertim string a enter i l'afegim al array.
+			}
+			line = reader.ReadLine ();
+		}
+		reader.Close();
+	}
+}
