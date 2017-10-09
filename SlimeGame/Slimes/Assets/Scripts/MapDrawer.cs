9d948ee726b586563e7a1d0ffb679fd76b0ace0d
m5m7n5 <sporemaniaco@gmail.com>
2017-10-10 00:19:17 +0200

    [WIP] Working in mapdrawer

---
+++ SlimeGame/Slimes/Assets/Scripts/MapDrawer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDrawer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	private void instantiateMap(System.Collections.IEnumerable map){
+		foreach (MapCoordinates tile in map) {
+			
+		}
+	}
+
+	public interface MapCoordinates{
+		Vector2 getPosition();
+
+	}
+
+}
