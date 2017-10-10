f41f44d41f5845644eabe4c3423efd21d38513f9
Marcos Buenacasa López <sporemaniaco@gmail.com>
2017-10-10 18:31:37 +0200

    [FEAT] Drawing map and added some controllers

---
+++ SlimeGame/Slimes/Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	private GameObject selectedItem;
+
+	// Use this for initialization
+	void Start () {
+		MapDrawer.InitTest ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- SlimeGame/Slimes/Assets/Scripts/MapDrawer.cs
+++ SlimeGame/Slimes/Assets/Scripts/MapDrawer.cs
@@ -2,40 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapDrawer : MonoBehaviour {
+public class MapDrawer {
 
-	private Vector2 horizontalOffset;
-	private Vector2 diagonalOffset;
-	private Vector2 verticalOffset;
+	private static Vector2 horizontalOffset;
+	private static Vector2 diagonalOffset;
+	private static Vector2 verticalOffset;
 
 	// Use this for initialization
-	void Start () {
-		
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public static void InitTest () {
+		testDrawer ();
 	}
 		
-	public void instantiateMap(System.Collections.IEnumerable map){
+	public static void instantiateMap(System.Collections.IEnumerable map){
 
-		horizontalOffset = new Vector2 (1f, 0f);
-		diagonalOffset = new Vector2 (0.5f, 0.5f);
-		verticalOffset = new Vector2 (0f, 1f);
+		Sprite sprite = Resources.Load<Sprite>("testTileFlat");
+		horizontalOffset = new Vector2 (sprite.rect.width/(float)(sprite.pixelsPerUnit*2f), 0f);
+		diagonalOffset = new Vector2 (sprite.rect.width/(float)(sprite.pixelsPerUnit*4f), 3f*sprite.rect.height/(float)(sprite.pixelsPerUnit*8));
+		verticalOffset = new Vector2 (0f, 3f*sprite.rect.height/(float)(sprite.pixelsPerUnit*4));
 
 		foreach (MapCoordinates tile in map) {
 			Vector2 tileWorldPosition = new Vector2 ();
 			int x = (int)tile.getPosition().x;
 			int y = (int)tile.getPosition().y;
-			if (x % 2 == 1) {
+			if (x % 2 == 1 && x!=0) {
 				tileWorldPosition += diagonalOffset;	 
 			}
-			tileWorldPosition += x * verticalOffset;
+			tileWorldPosition += x/2 * verticalOffset;
 			tileWorldPosition += y * horizontalOffset;
-			GameObject newTile = new GameObject ("("+x+","+y+")");
-			newTile.transform.position = new Vector3 (tileWorldPosition.x, tileWorldPosition.y, 0f); 
+			GameObject newTile = new GameObject ("Tile ("+x+","+y+")");
+			newTile.AddComponent<SpriteRenderer> ();
+			newTile.GetComponent<SpriteRenderer> ().sprite = sprite;
+			newTile.transform.localScale = new Vector3 (0.5f, 0.5f, 1f);
+			newTile.transform.position = new Vector3 (tileWorldPosition.x, -tileWorldPosition.y, 0f); 
 		}
 
 	}
@@ -51,13 +49,14 @@ public class MapDrawer : MonoBehaviour {
 	/***
 	Class and methods for testing
 	***/
-	private void testDrawer(){
+	private static void testDrawer(){
 		List<TestClass> list = new List<TestClass> ();
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
 				list.Add (new TestClass (new Vector2((float)i,(float)j)));
 			}
 		}
+		instantiateMap (list);
 	}
 
 	private class TestClass: MapCoordinates{
