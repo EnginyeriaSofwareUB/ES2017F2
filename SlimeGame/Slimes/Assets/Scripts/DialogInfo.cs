30cb6ef825038de0ddee4a28bcbef77241271457
Ignasi GM <ignasiguillen@gmail.com>
2017-10-13 18:17:36 +0200

    Panell amb informació pel jugador

---
+++ SlimeGame/Slimes/Assets/Scripts/DialogInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogInfo : MonoBehaviour {
+	
+	public GameObject Panel;
+	private int counter;
+
+	public void showhidePanel(){
+		counter++;
+		if (counter % 2 == 1) {
+			Panel.gameObject.SetActive (false);
+		} else {
+			Panel.gameObject.SetActive (true);
+		}
+
+	}
+
+}
