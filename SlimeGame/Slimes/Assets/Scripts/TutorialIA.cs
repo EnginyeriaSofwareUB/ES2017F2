699bd8d0713d4b6f41f864b77e61617f44903c88
TuTu94 <ctutum@hotmail.com>
2017-11-22 16:50:16 +0100

    Add IA

---
+++ SlimeGame/Slimes/Assets/Scripts/TutorialIA.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialIA : AIInterface {
+
+
+    public SlimeAction GetAction(GameController gameController) {
+        gameController.SetSelectedSlime(gameController.GetCurrentPlayer().GetSlimes()[0]);
+        return new SlimeAction(ActionType.CONQUER, gameController.GetCurrentPlayer().GetSlimes()[0].actualTile);
+    }
+}
